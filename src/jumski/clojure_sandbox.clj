3d83e7ccf072067b7d6c2f63db73dee6a666d8e7
Wojtek Majewski <jumski@gmail.com>
2014-05-18 22:31:00 +0200

    Initial

---
+++ src/jumski/clojure_sandbox.clj
@@ -0,0 +1,7 @@
+(ns jumski.clojure-sandbox
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/jumski/clojure_sandbox_test.clj
@@ -0,0 +1,7 @@
+(ns jumski.clojure-sandbox-test
+  (:require [clojure.test :refer :all]
+            [jumski.clojure-sandbox :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
